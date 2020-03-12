68e7d828f54be11c41b4794bba6abb2d414f1448
Casper Broekhuizen <broekhuizen.casper@gmail.com>
2020-03-12 15:08:24 +0100

    Initial commit

---
+++ ADB.h
@@ -0,0 +1,44 @@
+/*
+ * ADCS.h
+ *
+ *  Created on: 23 Jul 2019
+ *      Author: stefanosperett
+ */
+
+#ifndef ADB_H_
+#define ADB_H_
+
+#include <ADBCTelemetryContainer.h>
+#include <driverlib.h>
+#include "SLOT_SELECT.h"
+#include "SoftwareUpdateService.h"
+#include "Bootloader.h"
+#include "msp.h"
+#include "DelfiPQcore.h"
+#include "PQ9Bus.h"
+#include "PQ9Frame.h"
+#include "DWire.h"
+#include "INA226.h"
+#include "DSerial.h"
+#include "CommandHandler.h"
+#include "PingService.h"
+#include "ResetService.h"
+#include "Task.h"
+#include "PeriodicTask.h"
+#include "TaskManager.h"
+#include "HousekeepingService.h"
+#include "TMP100.h"
+#include "DSPI.h"
+#include "MB85RS.h"
+#include "TestService.h"
+#include "PeriodicTaskNotifier.h"
+
+#define FCLOCK 48000000
+
+#define ADCS_ADDRESS     5
+
+// callback functions
+void acquireTelemetry(OBCTelemetryContainer *tc);
+void periodicTask();
+
+#endif /* ADB_H_ */
---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include "ADB.h"
+
+// I2C bus
+DWire I2Cinternal(0);
+INA226 powerBus(I2Cinternal, 0x40);
+INA226 torquerX(I2Cinternal, 0x41);
+INA226 torquerY(I2Cinternal, 0x42);
+INA226 torquerZ(I2Cinternal, 0x43);
+TMP100 temp(I2Cinternal, 0x48);
+
+// SPI bus
+DSPI spi(3);
+//MB85RS fram(spi, GPIO_PORT_P1, GPIO_PIN0 );
+
+// Bootloader
+//Bootloader bootLoader = Bootloader(fram);
+
+// CDHS bus handler
+PQ9Bus pq9bus(3, GPIO_PORT_P9, GPIO_PIN0);
+
+// debug console handler
+DSerial serial;
+
+// services running in the system
+PingService ping;
+//ResetService reset( GPIO_PORT_P4, GPIO_PIN0, &fram );
+ResetService reset( GPIO_PORT_P4, GPIO_PIN0)
+HousekeepingService<OBCTelemetryContainer> hk;
+//SoftwareUpdateService SWupdate(fram);
+Service* services[] = { &ping, &reset, &hk};
+
+// ADCS board tasks
+CommandHandler<PQ9Frame> cmdHandler(pq9bus, services, 3);
+Task timerTask(periodicTask);
+Task* periodicTasks[] = {&timerTask};
+PeriodicTaskNotifier taskNotifier = PeriodicTaskNotifier(FCLOCK, periodicTasks, 1);
+Task* tasks[] = { &timerTask, &cmdHandler };
+
+volatile bool cmdReceivedFlag = false;
+DataFrame* receivedFrame;
+
+// system uptime
+unsigned long uptime = 0;
+
+// TODO: remove when bug in CCS has been solved
+void receivedCommand(DataFrame &newFrame)
+{
+    cmdReceivedFlag = true;
+    receivedFrame = &newFrame;
+    //cmdHandler.received(newFrame);
+}
+
+void validCmd(void)
+{
+    reset.kickInternalWatchDog();
+}
+
+void periodicTask()
+{
+    // increase the timer, this happens every second
+    uptime++;
+
+    // collect telemetry
+    hk.acquireTelemetry(acquireTelemetry);
+
+    // refresh the watch-dog configuration to make sure that, even in case of internal
+    // registers corruption, the watch-dog is capable of recovering from an error
+    reset.refreshConfiguration();
+
+    // kick hardware watch-dog after every telemetry collection happens
+    reset.kickExternalWatchDog();
+
+}
+
+void acquireTelemetry(OBCTelemetryContainer *tc)
+{
+
+}
+
+/**
+ * main.c
+ */
+void main(void)
+{
+    // initialize the MCU:
+    // - clock source
+    // - clock tree
+    DelfiPQcore::initMCU();
+
+    // Initialize I2C master
+    I2Cinternal.setFastMode();
+    I2Cinternal.begin();
+
+    // Initialize SPI master
+    spi.initMaster(DSPI::MODE0, DSPI::MSBFirst, 1000000);
+    //fram.init();
+
+    // initialize the shunt resistor
+    powerBus.setShuntResistor(40);
+    torquerX.setShuntResistor(40);
+    torquerY.setShuntResistor(40);
+    torquerZ.setShuntResistor(40);
+
+    // initialize temperature sensor
+    temp.init();
+
+    // initialize the console
+    serial.begin( );                        // baud rate: 9600 bps
+    pq9bus.begin(115200, 3);     // baud rate: 115200 bps
+                                            // address ADB (3)
+
+    //InitBootLoader!
+    //bootLoader.JumpSlot();
+
+    // initialize the reset handler:
+    // - prepare the watch-dog
+    // - initialize the pins for the hardware watch-dog
+    // - prepare the pin for power cycling the system
+    reset.init();
+
+    // link the command handler to the PQ9 bus:
+    // every time a new command is received, it will be forwarded to the command handler
+    // TODO: put back the lambda function after bug in CCS has been fixed
+    //pq9bus.setReceiveHandler([](PQ9Frame &newFrame){ cmdHandler.received(newFrame); });
+    pq9bus.setReceiveHandler(&receivedCommand);
+
+    // every time a command is correctly processed, call the watch-dog
+    // TODO: put back the lambda function after bug in CCS has been fixed
+    //cmdHandler.onValidCommand([]{ reset.kickInternalWatchDog(); });
+    //cmdHandler.onValidCommand(&validCmd);
+
+    serial.print("ADB booting...SLOT: ");
+        serial.println(Bootloader::getCurrentSlot(), DEC);
+
+        if(HAS_SW_VERSION == 1){
+            serial.print("SW_VERSION: ");
+            serial.println((const char*)xtr(SW_VERSION));
+        }
+
+    TaskManager::start(tasks, 2);
+}
